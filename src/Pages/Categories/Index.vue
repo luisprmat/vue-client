26c98e48187ee971933f5d18b861e478c7ec34c1
Luis Parrado <luisprmat@gmail.com>
2024-02-04 04:13:46 -0500

    Show products cards without pagination

--- src/Pages/Categories/Index.vue
+++ src/Pages/Categories/Index.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>
-        Categorias
-    </div>
+    <main>
+        <div class="container">
+            <h2 class="display-6">Categorías</h2>
+        </div>
+    </main>
 </template>
--- src/Pages/Products/Index.vue
+++ src/Pages/Products/Index.vue
@@ -1,5 +1,38 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const products = ref([])
+
+const getProducts = async () => {
+  try {
+    const response = await fetch('http://api-laravel.test/api/products')
+    const data = await response.json()
+    products.value = data.data
+  } catch (e) {
+    console.log(e)
+  }
+}
+onMounted(getProducts)
+</script>
+
 <template>
   <main>
-    <div>Productos</div>
+    <div class="container">
+      <h2 class="display-6 my-4">Productos</h2>
+
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
+        <div v-for="product in products" :key="product.id" class="col">
+          <div class="card h-100">
+            <img src="http://placehold.it/300x300" class="card-img-top" :alt="product.name">
+            <div class="card-body">
+              <h5 class="card-title mb-2">{{ product.name }}</h5>
+              <h6 class="card-subtitle my-2 text-body-secondary">$ {{ product.price }}</h6>
+              <h6 class="card-subtitle my-2 text-primary fst-italic">{{ product.category.name }}</h6>
+              <p class="card-text">{{ product.description }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
