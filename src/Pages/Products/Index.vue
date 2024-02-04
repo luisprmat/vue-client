a0e42f79c564185f35c93f55d3be0a45346d0dd4
Luis Parrado <luisprmat@gmail.com>
2024-02-04 18:02:25 -0500

    Add pagination to products

--- src/Pages/Products/Index.vue
+++ src/Pages/Products/Index.vue
@@ -1,13 +1,14 @@
 <script setup>
 import { onMounted, ref } from 'vue';
+import { Bootstrap5Pagination } from 'laravel-vue-pagination';
 
-const products = ref([])
+const products = ref({})
 
-const getProducts = async () => {
+const getProducts = async (page = 1) => {
   try {
-    const response = await fetch(import.meta.env.VITE_BASE_URL + '/products')
-    const data = await response.json()
-    products.value = data.data
+    const response = await fetch(import.meta.env.VITE_BASE_URL + '/products?page=' + page)
+    products.value = await response.json()
+    console.log(products.value)
   } catch (e) {
     console.log(e)
   }
@@ -20,7 +21,7 @@ onMounted(getProducts)
     <h2 class="display-6 my-4">Productos</h2>
 
     <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
-      <div v-for="product in products" :key="product.id" class="col">
+      <div v-for="product in products.data" :key="product.id" class="col">
         <div class="card h-100">
           <img src="http://placehold.it/300x300" class="card-img-top" :alt="product.name">
           <div class="card-body">
@@ -31,6 +32,21 @@ onMounted(getProducts)
           </div>
         </div>
       </div>
+    </div>
+
+    <div class="my-4 d-flex flex-column flex-md-row justify-content-md-between align-items-md-baseline">
+      <div class="order-last order-md-first">
+        <p class="text-sm text-gray-700 leading-5 dark:text-gray-200">
+          <span>Mostrando </span>
+          <span class="fw-medium">{{ products.meta?.from }}</span>
+          <span> al </span>
+          <span class="fw-medium">{{ products.meta?.to }}</span>
+          <span> de </span>
+          <span class="fw-medium">{{ products.meta?.total }}</span>
+          <span> resultados</span>
+        </p>
+      </div>
+      <Bootstrap5Pagination :data="products" @pagination-change-page="getProducts" />
     </div>
   </div>
 </template>
