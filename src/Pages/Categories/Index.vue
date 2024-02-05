f365d788cd9c6f50a8e676263547e9797a3038a4
Luis Parrado <luisprmat@gmail.com>
2024-02-04 21:31:21 -0500

    Add bootstrap icons to edit and delete

--- src/Pages/Categories/Index.vue
+++ src/Pages/Categories/Index.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { onMounted, ref } from 'vue';
+import noImage from '@/assets/no-image-available.png';
 
 const categories = ref({})
 
@@ -21,12 +22,17 @@ onMounted(getCategories)
     <div class="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-6 g-4">
       <div v-for="category in categories.data" :key="category.id" class="col">
         <div class="card h-100">
-          <img :src="category.photo_url ? category.photo_url : '/src/assets/no-image-available.png'" class="card-img-top"
-            :alt="category.name">
+          <img :src="category.photo_url ? category.photo_url : noImage" class="card-img-top" :alt="category.name">
           <div class="card-body">
             <h5 class="card-title mb-2">{{ category.name }}</h5>
             <h6 class="card-subtitle my-2 text-primary fst-italic">Productos: {{ category.products }}</h6>
             <p class="card-text">ID: {{ category.id }}</p>
+          </div>
+          <div class="card-footer text-body-secondary d-flex justify-content-between">
+            <button class="btn btn-outline-primary btn-sm"><i class="bi bi-pencil-square"></i>
+            </button>
+            <button class="btn btn-outline-danger btn-sm"><i class="bi bi-trash"></i>
+            </button>
           </div>
         </div>
       </div>
